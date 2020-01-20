0dd69181e5c169a96df106f1202258006084427a
Chris DeMartini <chris@datablick.com>
2020-01-20 10:38:27 -0800

    chore: initial render of h3 as geojson on map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState } from 'react';
 import './App.css';
+import MapGL, { Source, Layer } from 'react-map-gl';
+import { bayAreaHexes, bayAreaHexesBoundaries, brazilHexesBoundaries, brazilHexMap } from './h3Examples';
+// import * as turf from '@turf/turf';
+// import * as h3 from 'h3-js';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGVtYXJ0c2MiLCJhIjoiY2o4bjd4aG40MTZudTJ3bXo5aDU3dmFpMSJ9.O72JlexJ3-xy030RqsoSgA'; // Set your mapbox token here
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14,
+    bearing: 0,
+    pitch: 0
+  });
+  // console.log('checking turf result', h3.polyfill(turf.bboxPolygon([-73.9872354804, -33.7683777809, -34.7299934555, 5.24448639569]).geometry.coordinates,4));
+
+
+  const hexLayer = { 
+    id: 'h3_hex_layer',
+    type: 'fill',
+    layout: {},
+    paint: {
+    'fill-color': '#088',
+    'fill-opacity': 0.8
+    }
+  }
+
+  const BrazilLayer = { 
+    id: 'h3_hex_layer_brazil',
+    type: 'fill',
+    layout: {},
+    paint: {
+    'fill-color': {
+      property: 'value',
+      stops: [[0, '#fff'], [.5, '#f00']]
+      },
+      'fill-opacity': 0.6
+    }
+  }
+
+  console.log('bay area hexes', brazilHexMap);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/light-v9"
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Source id="bay-area" type="geojson" data={bayAreaHexesBoundaries}>
+          <Layer {...hexLayer} />
+        </Source>
+        <Source id="brazil-data" type="geojson" data={brazilHexesBoundaries}>
+          <Layer {...BrazilLayer} />
+        </Source>
+      </MapGL>
       </header>
     </div>
   );
---
+++ src/h3Examples.js
@@ -0,0 +1,54 @@
+import * as h3 from 'h3-js';
+import geojson2h3 from 'geojson2h3';
+import * as turf from '@turf/turf';
+
+export const h3index = h3.geoToH3(37.77, -122.43, 9)
+export const h3center = h3.h3ToGeo(h3index)
+export const h3outline = h3.h3ToGeoBoundary(h3index)
+
+export const res = h3.h3GetResolution(h3index)
+export const baseCell = h3.h3GetBaseCell(h3index)
+export const valid = h3.h3IsValid(h3index)
+export const resIsClassIII = h3.h3IsResClassIII(h3index)
+export const isPentagon = h3.h3IsPentagon(h3index)
+export const neighbors = h3.kRing(h3index, 1)
+export const threeRings = h3.kRingDistances(h3index, 3)
+export const ring2Away = h3.hexRing(h3index, 2)
+export const index2 = h3.geoToH3(38.88, -122.34, 9)
+export const hexLine = h3.h3Line(h3index, index2)
+export const hexDistance = h3.h3Distance(h3index, index2)
+export const coords = h3.experimentalH3ToLocalIj(h3index, index2)
+export const index2again = h3.experimentalLocalIjToH3(h3index, coords)
+export const parent = h3.h3ToParent(h3index, 8)
+export const immediateChildren = h3.h3ToParent(h3index, 10)
+export const packedRings = h3.compact([].concat(...threeRings))
+export const unpackedRings = h3.uncompact(packedRings, 9)
+export const bayAreaHexes = h3.polyfill([
+    [37.77, -122.43],
+    [37.55, -122.43],
+    [37.55, -122.23],
+    [37.77, -122.23],
+    [37.77, -122.43],
+  ], 7)
+export const bayAreaHexesBoundaries = geojson2h3.h3SetToFeatureCollection(bayAreaHexes);
+
+export const brazilHexes = h3.polyfill(turf.bboxPolygon([-33.7683777809, -34.7299934555, 5.24448639569, -73.9872354804]).geometry.coordinates,4);
+export const brazilHexMap = brazilHexes.reduce((res, hexagon) => ({...res, [hexagon]: Math.random()}), {});
+export const brazilHexesBoundaries = geojson2h3.h3SetToFeatureCollection(
+    Object.keys(brazilHexMap), hex => ({ value: brazilHexMap[hex]})
+);
+
+// export const bayAreaHexesBoundaries = bayAreaHexes.map(hex => {
+//     const tmp = h3.h3ToGeoBoundary(hex);
+//     tmp.push(tmp[0]);
+
+//     return (
+//     {
+//         type: 'Feature',
+//         properties:{},
+//         geometry: {
+//             'type': 'Polygon',
+//             'coordinates': [tmp]
+//         }
+//     });
+// });
